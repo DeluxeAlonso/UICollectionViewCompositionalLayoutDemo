28c7e11932fe0396bd9d74dde9b5504cff09cd64
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-08-18 22:36:46 -0500

    Removes unnecessary code from app delegate and scene delegate

--- UICollectionViewCompositionalLayoutDemo/SceneDelegate.swift
+++ UICollectionViewCompositionalLayoutDemo/SceneDelegate.swift
@@ -15,8 +15,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
-        let navigationController = UINavigationController(rootViewController: LayoutsViewController(viewModel: LayoutsViewModel()))
-        self.window!.rootViewController = navigationController
+        
+        let layoutsViewController = LayoutsViewController(viewModel: LayoutsViewModel())
+        let navigationController = UINavigationController(rootViewController: layoutsViewController)
+        
+        self.window?.rootViewController = navigationController
         self.window?.makeKeyAndVisible()
     }
     
