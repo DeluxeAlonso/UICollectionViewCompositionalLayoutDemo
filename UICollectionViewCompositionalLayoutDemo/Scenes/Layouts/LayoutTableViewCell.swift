e935542697efb466bc2de6df471825b6c8c2c3b6
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-08-18 22:31:33 -0500

    Adds minor fix to layout call not showing the complete detail text

--- UICollectionViewCompositionalLayoutDemo/Scenes/Layouts/LayoutTableViewCell.swift
+++ UICollectionViewCompositionalLayoutDemo/Scenes/Layouts/LayoutTableViewCell.swift
@@ -10,12 +10,21 @@ import UIKit
 
 class LayoutTableViewCell: UITableViewCell {
     
+    // MARK: - Initializers
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        setupUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private
+    
+    private func setupUI() {
+        detailTextLabel?.numberOfLines = 0
     }
 
 }
