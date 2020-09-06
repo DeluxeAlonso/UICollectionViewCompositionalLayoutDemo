a314c295db5cf72d22006af8a3394d1199fd7659
DeluxeAlonso <alonso.alvarez@pucp.pe>
2020-09-05 22:34:53 -0500

    Creates type erasing struct for layout cell view model to be used with table view diffable data source

---
+++ UICollectionViewCompositionalLayoutDemo/Scenes/Layouts/ViewComponents/AnyLayoutCellViewModel.swift
@@ -0,0 +1,9 @@
+//
+//  AnyLayoutCellViewModel.swift
+//  UICollectionViewCompositionalLayoutDemo
+//
+//  Created by Alonso on 9/5/20.
+//  Copyright © 2020 Alonso. All rights reserved.
+//
+
+import Foundation
