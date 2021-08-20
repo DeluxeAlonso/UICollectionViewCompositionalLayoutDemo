9c0067da7ac3d4a2e2d6bd569595482c09dc777f
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-08-20 01:20:49 -0500

    Creates file for layout style I

---
+++ UICollectionViewCompositionalLayoutDemo/CompositionalLayouts/CompositionalLayoutI.swift
@@ -0,0 +1,9 @@
+//
+//  CompositionalLayoutI.swift
+//  UICollectionViewCompositionalLayoutDemo
+//
+//  Created by Alonso on 20/08/21.
+//  Copyright © 2021 Alonso. All rights reserved.
+//
+
+import Foundation
