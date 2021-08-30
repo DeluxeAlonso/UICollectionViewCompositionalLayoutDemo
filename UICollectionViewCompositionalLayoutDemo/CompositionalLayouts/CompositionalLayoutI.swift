2b25254cca2b42b2df48aedd2585662970132146
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-08-30 01:04:54 -0500

    Implementa create method for CompositionalLayoutI

--- UICollectionViewCompositionalLayoutDemo/CompositionalLayouts/CompositionalLayoutI.swift
+++ UICollectionViewCompositionalLayoutDemo/CompositionalLayouts/CompositionalLayoutI.swift
@@ -19,7 +19,34 @@ struct CompositionalLayoutI: CompositionalLayoutProtocol {
     }
 
     func create() -> UICollectionViewLayout {
-        fatalError()
+        let layout = UICollectionViewCompositionalLayout { (section: Int, environment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+            let leadingItem = NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: NSCollectionLayoutDimension.fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)))
+            leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+            let leadingGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.7),
+                                                          heightDimension: .fractionalHeight(1))
+            let leadingGroup = NSCollectionLayoutGroup.vertical(layoutSize: leadingGroupSize,
+                                                                subitem: leadingItem, count: 1)
+
+            let trailingItem =  NSCollectionLayoutItem(layoutSize: NSCollectionLayoutSize(widthDimension: NSCollectionLayoutDimension.fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0)))
+            trailingItem.contentInsets = NSDirectionalEdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 5)
+            let trailingGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalHeight(1))
+
+            let trailingGroup =  NSCollectionLayoutGroup.vertical(layoutSize: trailingGroupSize, subitem: trailingItem, count: 2)
+
+            let containerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                            heightDimension: .absolute(250))
+
+            let containerGroup = NSCollectionLayoutGroup.horizontal(layoutSize: containerGroupSize,
+                                                                    subitems: [leadingGroup, trailingGroup])
+
+
+            let section = NSCollectionLayoutSection(group: containerGroup)
+            section.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 0, bottom: 20, trailing: 0)
+
+            return section
+        }
+
+        return layout
     }
 
 }
