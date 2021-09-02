4dca1089d15caa776b44514a41cb88773b728f9f
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-09-02 00:16:34 -0500

    Implements LayoutsViewFactory

--- UICollectionViewCompositionalLayoutDemo/Scenes/Layouts/LayoutsViewFactory.swift
+++ UICollectionViewCompositionalLayoutDemo/Scenes/Layouts/LayoutsViewFactory.swift
@@ -13,3 +13,19 @@ protocol LayoutsViewFactoryProtocol {
     var layouts: [CompositionalLayoutProtocol] { get }
 
 }
+
+struct LayoutsViewFactory: LayoutsViewFactoryProtocol {
+
+    var layouts: [CompositionalLayoutProtocol] {
+        return [CompositionalLayoutA(),
+                CompositionalLayoutB(),
+                CompositionalLayoutC(),
+                CompositionalLayoutD(),
+                CompositionalLayoutE(),
+                CompositionalLayoutF(),
+                CompositionalLayoutG(),
+                CompositionalLayoutH(),
+                CompositionalLayoutI()]
+    }
+
+}
