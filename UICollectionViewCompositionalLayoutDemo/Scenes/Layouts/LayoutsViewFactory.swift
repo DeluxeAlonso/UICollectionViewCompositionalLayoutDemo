55d0003d2ecd9a60a7232ed6459ad22e0e172f8a
DeluxeAlonso <alonso.alvarez@pucp.pe>
2021-09-01 23:57:55 -0500

    Creates LayoutsViewFactoryProtocol

---
+++ UICollectionViewCompositionalLayoutDemo/Scenes/Layouts/LayoutsViewFactory.swift
@@ -0,0 +1,13 @@
+//
+//  LayoutsViewFactory.swift
+//  UICollectionViewCompositionalLayoutDemo
+//
+//  Created by Alonso on 1/09/21.
+//  Copyright © 2021 Alonso. All rights reserved.
+//
+
+import Foundation
+
+protocol LayoutsViewFactoryProtocol {
+    
+}
